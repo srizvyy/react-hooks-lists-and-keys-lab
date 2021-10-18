76f735559deb4d8c6822c02422f01a95e15df824
Saim Rizvi <shr2mz@gmail.com>
2021-10-17 21:42:11 -0400

    commit tree 853cea59ab1678de6034821543c6cfd611272a70

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,19 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  links.map((item) => {
+    return (
+      <a key={item} href="">{item}</a>
+    )
+  })
+  return ( 
+  <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#projects">Projects</a>
+  </nav>
+  )
 }
+
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+     
   return (
     <div className="project-item">
       <h3>{name}</h3>
